680ae4531989a0353d10b96a321151474bc7c9ae
MrsKing2023 <smarshall84@yahoo.com>
2023-09-11 21:07:21 -0400

    work on writing tests

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -1,11 +1,25 @@
 package org.launchcode;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 class CarTest {
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    @Test
+    public void emptyTest() {
+        assertEquals(10, 10, .001);
+    }
     //TODO: constructor sets gasTankLevel properly
+    @Test
+    public void testInitialGasTank() {
+        Car test_car;
+        
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
     //TODO: gasTankLevel is accurate after driving within tank range
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
 }
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -1,13 +1,89 @@
 package org.launchcode;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class BalancedBracketsTest {
     //TODO: add tests here
-    @Test
+    //TODO: Test 1
+    @BeforeEach
     public void emptyTest() {
         assertEquals(true, true);
     }
 }
+
+    //TODO: Test 2
+   // @Test
+    //public void onlyBracketsReturnsTrue() {
+      //  assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+   // }
+//msg =
+//expected =
+//actual =
+
+//TODO: Test 3
+// @Test
+//public void
+
+//msg =
+//expected =
+//actual =
+//TODO: Test 4
+// @Test
+//public void
+
+//msg =
+//expected =
+//actual =
+//TODO: Test 5
+// @Test
+//public void
+
+//msg =
+//expected =
+//actual =
+//TODO: Test 6
+// @Test
+//public void
+
+//msg =
+//expected =
+//actual =
+//TODO: Test 7
+// @Test
+//public void
+//msg =
+//expected =
+//actual =
+//TODO: Test 8
+// @Test
+//public void
+//msg =
+//expected =
+//actual =
+//TODO: Test 9
+// @Test
+//public void
+//msg =
+//expected =
+//actual =
+//TODO: Test 10
+// @Test
+//public void
+//msg =
+//expected =
+//actual =
+//TODO: Test 11
+// @Test
+//public void
+//msg =
+//expected =
+//actual =
+//TODO: Test 12
+// @Test
+//public void
+//msg =
+//expected =
+//actual =
